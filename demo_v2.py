28d78edd883bb40c4e61bf1c72ac147e6748f6ca
shilapi <shilapi@outlook.com>
2024-09-03 19:07:55 +0800

    Add v2 demo back

---
+++ demo_v2.py
@@ -0,0 +1,32 @@
+# Description: This is a demo script to show how to use the pydglab library to interact with the DGLab device.
+
+import asyncio
+import logging
+
+import pydglab
+from pydglab import model
+
+logging.basicConfig(
+    format="%(module)s [%(levelname)s]: %(message)s", level=logging.INFO
+)
+
+
+async def _():
+    await pydglab.scan()
+    dglab_instance = pydglab.dglab()
+    try:
+        await dglab_instance.create()
+    except TimeoutError:
+        logging.error("Timeout, retrying...")
+        await dglab_instance.create()
+    await dglab_instance.get_strength()
+    await dglab_instance.set_strength_sync(1, 1)
+    await dglab_instance.set_wave_sync(0, 0, 0, 0, 0, 0)
+    await dglab_instance.set_wave_set(model.Wave_set["Going_Faster"], model.ChannelA)
+    await dglab_instance.get_batterylevel()
+    await dglab_instance.get_strength()
+    await asyncio.sleep(2)
+    await dglab_instance.close()
+
+
+asyncio.run(_())
