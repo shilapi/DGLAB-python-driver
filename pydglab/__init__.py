4f5b64ee78ab5cb7610c19bb2511d99db6095b5d
Shilapi Chen <shilapi@outlook.com>
2024-09-03 17:18:53 +0800

    Update __init__.py

--- pydglab/__init__.py
+++ pydglab/__init__.py
@@ -10,6 +10,8 @@ if bool(os.environ.get("BLEAK_LOGGING", False)):
     handler.setFormatter(logging.Formatter(fmt=LOGFORMAT))
     _logger.addHandler(handler)
 
-from .service import dglab_v3
+from .service import dglab, dglab_v3
+from .model import *
+from .bthandler import scan
 from .model_v3 import *
 from .bthandler_v3 import scan
